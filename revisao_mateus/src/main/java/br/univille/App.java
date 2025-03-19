e81061ab1e6b6cf1414066737b9d325cbcef2552
Mateus Medeiros dos Santos <106558452+Th3FIa5h@users.noreply.github.com>
2025-03-18 21:51:21 -0300

    Feat: Revisao Objeto Cidade

--- revisao_mateus/src/main/java/br/univille/App.java
+++ revisao_mateus/src/main/java/br/univille/App.java
@@ -1,11 +1,21 @@
 package br.univille;
+
+import br.univille.entity.Cidade;
+import br.univille.entity.Pessoa;
+
 public class App {
     public static void main(String[] args) {
         //System.out.println("Hello, World!");
         //new = Instância
+        Cidade jlle = new Cidade();
+        jlle.setNome("Joinville");
+        jlle.setEstado("Santa Catarina");
+
+
+        Pessoa mariazinha = new Pessoa("Mariazinha");
         Pessoa zezinho = new Pessoa();
-        Pessoa mariazinha = new Pessoa("Mariazinha"); 
         zezinho.setNome("Zezinho");
+        zezinho.setCidade(jlle);
 
         System.out.println(mariazinha);
         System.out.println(zezinho);
--- revisao_mateus/src/main/java/br/univille/entity/Pessoa.java
+++ revisao_mateus/src/main/java/br/univille/entity/Pessoa.java
@@ -1,13 +1,42 @@
-package br.univille;
+package br.univille.entity;
 public class Pessoa {
     //atributo (variável)
+    private long id;
     private String nome;
+    private String endereco;
     
+    private Cidade cidade;
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
     //Contrutor (mesmo nome da classe, não tem retorno)
     public Pessoa(String nome) {
         //this referencia a classe
         this.nome = nome;
     }
+
     //Dois métodos com assinatura semelhante = polimorfismo
     public Pessoa() {
         
@@ -22,8 +51,13 @@ public class Pessoa {
         this.nome = nome;
     }
     //Sobreescrita de metódo (overwrite)
+    @Override
     public String toString(){
         return getNome();
     }
+
+
+
+    
 
 }
