a624e5a7db9f7de01fc62d8f41e812e9f0a63217
Mateus Medeiros dos Santos <106558452+Th3FIa5h@users.noreply.github.com>
2025-04-01 22:00:32 -0300

    Feat: projeto principal

---
+++ projetofabsoftfinancas/src/main/java/br/univille/projetofabsoftfinancas/ProjetofabsoftfinancasApplication.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftfinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofabsoftfinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofabsoftfinancasApplication.class, args);
+	}
+
+}
---
+++ projetofabsoftfinancas/src/test/java/br/univille/projetofabsoftfinancas/ProjetofabsoftfinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.projetofabsoftfinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofabsoftfinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
