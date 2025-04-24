859e67406bf7ea9e2caebc9c881ba119ae7c3dd9
Medeiros <106558452+Th3FIa5h@users.noreply.github.com>
2025-04-24 13:03:00 -0300

    Feat: AlertaPagamentoService created

--- projetofabsoftfinancas/src/main/java/br/univille/projetofabsoftfinancas/service/AlertaPagamentoService.java
+++ projetofabsoftfinancas/src/main/java/br/univille/projetofabsoftfinancas/service/AlertaPagamentoService.java
@@ -1,5 +1,12 @@
 package br.univille.projetofabsoftfinancas.service;
 
-public class AlertaPagamentoService {
-    
+import java.util.List;
+
+import br.univille.projetofabsoftfinancas.entity.AlertaPagamento;
+
+public interface AlertaPagamentoService {
+    AlertaPagamento save(AlertaPagamento alertapagamento);
+    List<AlertaPagamento> getAll();
+    AlertaPagamento getById(long id);
+    AlertaPagamento delete(long id);
 }
