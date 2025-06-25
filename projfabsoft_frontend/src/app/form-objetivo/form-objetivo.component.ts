022bbef11cf9b5d3adb8f48c1f8f8135e120f641
NikolasAndreskiRodrigues <128433982+NikolasAndreskiRodrigues@users.noreply.github.com>
2025-06-25 13:08:55 +0000

    Feat Form-screen's and fix component.ts

--- projfabsoft_frontend/src/app/form-objetivo/form-objetivo.component.ts
+++ projfabsoft_frontend/src/app/form-objetivo/form-objetivo.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Objetivo } from '../model/objetivo';
+import { ObjetivoService } from '../service/objetivo.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form-objetivo',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './form-objetivo.component.html',
-  styleUrl: './form-objetivo.component.css'
+  styleUrl: './form-objetivo.component.css',
+  providers: [ObjetivoService, Router]
 })
 export class FormObjetivoComponent {
+  objetivo: Objetivo = new Objetivo();
+  constructor(
+    private objetivoService: ObjetivoService,
+    private router: Router,
+    private activedRoute: ActivatedRoute
+  ) {
+    const id = this.activedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.objetivoService.getObjetivoById(id)
+      .subscribe(objetivo => {
+        this.objetivo = objetivo;
+      });
+    }
+  }
 
+  salvar() {
+    this.objetivoService.saveObjetivo(this.objetivo)
+    .subscribe(res => {
+      this.router.navigate(['/objetivo']);
+    });
+  }
 }
--- projfabsoft_frontend/src/app/form-receita/form-receita.component.ts
+++ projfabsoft_frontend/src/app/form-receita/form-receita.component.ts
@@ -1,11 +1,38 @@
 import { Component } from '@angular/core';
+import { Receita } from '../model/receita';
+import { ReceitaService } from '../service/receita.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form-receita',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './form-receita.component.html',
-  styleUrl: './form-receita.component.css'
+  styleUrl: './form-receita.component.css',
+  providers: [ReceitaService, Router]
 })
 export class FormReceitaComponent {
+  receita: Receita = new Receita();
+  constructor(
+    private receitaService: ReceitaService,
+    private router: Router,
+    private activedRoute: ActivatedRoute
+  ) {
+    const id = this.activedRoute.snapshot.paramMap.get('id');
+    if (id) {
+      this.receitaService.getReceitaById(id)
+      .subscribe(receita => {
+        this.receita = receita;
+      });
+    }
+  }
 
+  salvar() {
+    this.receitaService.saveReceita(this.receita)
+    .subscribe(res => {
+      this.router.navigate(['/receita']);
+    });
+  }
 }
